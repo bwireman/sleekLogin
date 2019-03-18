3f3e6dc0df8c090008ef2cf3911ecd4e3a28548a
bwireman <bwireman1@gmail.com>
2019-03-18 17:30:14 -0400

    removed moment

--- js/main.jsx
+++ js/main.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Likes from './likes';
 
 ReactDOM.render(
-  <Likes url="/api/v1/p/1/likes/" />,
   document.getElementById('reactEntry'),
 );
 
